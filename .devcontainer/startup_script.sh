2e32f05b74968a40abcb94f81751ae5b7aba34e4
codebuffdev <codebuffdev@gmail.com>
2023-09-06 20:17:10 +0530

    docker playground setup-v1

---
+++ .devcontainer/startup_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update -y
+sudo apt install docker-ce
+sudo usermod -aG docker $USER
+
+# Create a Docker volume
+docker volume create portainer_data
+
+# Run the Portainer container
+docker run -d -p 8000:8000 -p 9443:9443 \
+    --name portainer --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
