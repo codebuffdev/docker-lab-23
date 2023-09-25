9a6108e6146263429d443c05e7f0c26c26825e90
codebuffdev <codebuffdev@gmail.com>
2023-09-25 18:14:47 +0530

    docker playground setup-v2.2

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -9,15 +9,3 @@ sudo usermod -aG docker $USER
 
 echo "docker is set-up & running"
 
-# Create a Docker voluem 
-docker volume create portainer_data
-
-# Create a Docker network bridge for isolation
-docker network create portainer-net
-
-# Start Portainer container within the Codespace
-docker run -d -p 8000:8000 -p 9443:9443 \
-    --name portainer --network portainer-net --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-    portainer/portainer-ce:latest
